65271523a68eff17b43c1cdf32eaaaf1bb42a3b1
5Cyeop <124247182+5Cyeop@users.noreply.github.com>
2025-11-06 18:43:47 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,204 +5,208 @@ using namespace cv;
 
 int main()
 {
-    /*
-    Mat imgs[3];
-    for (int i = 0; i < 3; i++)
-    {
-        String filename = format("img%d.jpg", i + 1);
-        imgs[i] = imread(filename);
+	/*
+	Mat img1 = imread("lenna256.bmp", 0);
+	Mat img2 = imread("lenna256.bmp", 0);
+	
+	for (int j = 0; j < img1.rows; j++)
+	{
+		for (int i = 0; i < img1.cols; i++)
+		{
+			if (j < 10 || j > img1.rows - 10 || i < 10 || i > img1.cols - 10)
+			{
+				img1.data[img1.cols * j + i] = 0;
+			}
+		}
+	}
 
-        imshow(filename, imgs[i]);
-        waitKey();
-    }
-    
-    Mat img1;
-    //imshow("img1", img1);
+	for (int j = 0; j < img1.rows; j++)
+	{
+		for (int i = 0; i < img1.cols; i++)
+		{
+			if (j < 10 || j > img1.rows - 10 || i < 10 || i > img1.cols - 10)
+			{
+				img2.at<uchar>(j, i) = 0;
+			}
+		}
+	}
+
+	imwrite("test.bmp", img1);
+	imwrite("test2.bmp", img2);
+
+	waitKey();
+	*/
+
+	/*
+	void Mat1();
+	{
+		float data[] = { 1, 1, 2, 3 };
+		Mat mat1(2, 2, CV_32FC1, data);
+		cout << "mat1 : " << endl << mat1 << endl << endl;
+
+		Mat mat2 = mat1.inv();
+		cout << "mat2 : " << endl << mat2 << endl << endl;
+
+		cout << "mat1.t() : " << endl << mat1.t() << endl << endl;
+		cout << "mat1 + 3 : " << endl << mat1 + 3 << endl << endl;
+
+		cout << "mat1 + mat2 : " << endl << mat1 + mat2 << endl << endl;
+		cout << "mat1 * mat2 : " << endl << mat1 * mat2 << endl << endl;
+	}
+	*/
+
+	/*
+	uchar data1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+	Mat mat1(3, 4, CV_8UC1, data1);
+	Mat mat2 = mat1.reshape(0, 1);
+	Mat mat3 = mat1.reshape(0, 2);
+	Mat mat4 = mat1.reshape(0, 3);
+	Mat mat5 = mat1.reshape(0, 4);
+	//Mat mat6 = mat1.reshape(0, 5);
+
+	cout << "mat1: " << endl << mat1 << endl << endl;
+	cout << "mat2: " << endl << mat2 << endl << endl;
+	cout << "mat3: " << endl << mat3 << endl << endl;
+	cout << "mat4: " << endl << mat4 << endl << endl;
+	cout << "mat5: " << endl << mat5 << endl << endl;
+	//cout << "mat6: " << endl << mat6 << endl << endl;
+	*/
+
+	/*
+	void Mat2();
+	{
+		Mat img1 = imread("lenna.bmp", IMREAD_GRAYSCALE);
+		Mat img1f;
+		img1.convertTo(img1f, CV_32FC1);
+
+		uchar data1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+		Mat mat1(3, 4, CV_8UC1, data1);
+		Mat mat2 = mat1.reshape(0, 1);
+
+		cout << "mat1 : " << endl << mat1 << endl << endl;
+		cout << "mat2 : " << endl << mat2 << endl << endl;
+
+		Mat mat3 = Mat::ones(1, 4, CV_8UC1) * 255;
+		mat1.push_back(mat3);
+		cout << "mat3 : " << endl << mat1 << endl << endl;
+
+		mat1.resize(6, 100);
+		cout << "mat4 : " << endl << mat1 << endl << endl;
+		
+	}
+	*/
+
+	/*
+	Mat img1 = imread("lenna.bmp");
+	img1.at<Vec3b>(100, 50)[0] = 0;
+	img1.at<Vec3b>(100, 50)[1] = 255;
+	img1.at<Vec3b>(100, 50)[2] = 0;
+
+	imshow("lenna.bmp", img1);
+	*/
+
+	/*
+	Mat img2 = imread("lenna.bmp.", 1);
+	cout << img2.at<Vec3b>(100, 50) << endl;
+
+	uchar b = img2.at<Vec3b>(100, 50)[0];
+	uchar g = img2.at<Vec3b>(100, 50)[1];
+	uchar r = img2.at<Vec3b>(100, 50)[2];
+
+	cout << b << endl;
+	cout << g << endl;
+	cout << r << endl;
+	*/
+
+	/*
+	Mat img3 = imread("lenna.bmp");
+
+	
+	int a = (100 * img3.cols + 50) * 3;
+
+	img3.data[a + 0] = 0;   
+	img3.data[a + 1] = 255;
+	img3.data[a + 2] = 0;
+
+	
+	imwrite("lenna3.bmp", img3);
+	waitKey();
+	*/
+	
+	/*
+	Mat imga = imread("lenna.bmp");
+
+	for (int j = 0; j < imga.cols; j++)
+	{
+		for (int i = 0; i < imga.rows; i++)
+		{
+			if (j < 10 || j > imga.cols - 11 || i < 10 || i > imga.rows - 11)
+			{
+				imga.at<Vec3b>(i, j)[0] = 255;
+				imga.at<Vec3b>(i, j)[1] = 0;
+				imga.at<Vec3b>(i, j)[2] = 0;
+			}
+			
+		}
+	}
+
+	imwrite("lenna213.bmp", imga);
+	
+
+	Mat imgb = imread("lenna.bmp");
+
+	for (int j = 0; j < imga.cols; j++)
+	{
+		for (int i = 0; i < imga.rows; i++)
+		{
+			if (j < 10 || j > imga.cols - 11 || i < 10 || i > imga.rows - 11)
+			{
+				imgb.data[imgb.cols * 3 * j + (3 * i) + 0] = 255;
+				imgb.data[imgb.cols * 3 * j + (3 * i) + 1] = 0;
+				imgb.data[imgb.cols * 3 * j + (3 * i) + 2] = 0;
+			}
+
+		}
+	}
+
+	imwrite("lenna213-2.bmp", imga);
+	waitKey();
+	*/
+
+	Mat img = imread("lenna.bmp");
+
+	if (img.empty())
+	{
+		cerr << "Image load failed" << endl;
+		return -1;
+	}
+
+	while (1)
+	{
+		imshow("test", img);
+		int key = waitKey();
+
+		if (key == 'i')
+			img = ~img;
+
+		else if (key == 27 || key == 'q' || key == 'Q')
+			break;
+	}
 
 
-    Mat img2(480, 640, CV_8UC1);
-    Mat img3(480, 640, CV_8UC3);
-    Mat img4(Size(640, 480), CV_8UC3);
-
-    Mat img5(480, 640, CV_8UC1, Scalar(128));
-    Mat img6(480, 640, CV_8UC3, Scalar(0, 0, 255));
-
-    Mat mat1 = Mat::zeros(3, 3, CV_32FC1);
-    Mat mat2 = Mat::ones(3, 3, CV_32FC1);
-    Mat mat3 = Mat::eye(3, 3, CV_32FC1);
-
-    float data[] = { 1, 2, 3, 4, 5, 6 };
-    Mat mat4(2, 3, CV_32FC1, data);
-
-    Mat mat5 = (Mat_<float>(2, 3) << 1, 2, 3, 4, 5, 6);
-    Mat mat6 = Mat_<uchar>({ 2, 3 }, { 1, 2, 3, 4, 5, 6 });
-
-    //mat4.create(256, 256, CV_8UC3);
-    mat5.create(4, 4, CV_32FC1);
-
-    cout << "mat1" << endl << mat1 << endl << endl << 
-        "mat2" << endl << mat2 << endl << endl << 
-        "mat3" << endl << mat3 << endl << endl;
-    cout << "mat4" << endl << mat4 << endl;
-    //mat4 = Scalar(255, 0, 0);
-    //mat5.setTo(1.f);
-    
-
-    Mat img1 = imread("dog.bmp");   //dog 이미지 대입
-
-    Mat img2 = img1;    //이미지1 이미지2에 대입 -> dog -> yellow
-    Mat img3;
-    img3 = img1;    //이미지1 이미지3에 대입 -> dog -> yellow
-
-    Mat img4 = img1.clone();    //이미지4에 이미지1 clone -> dog -> 클론 데이터라 img1 변경에 영향 X
-    Mat img5;
-    img1.copyTo(img5);  //이미지5에 이미지1 copy -> dog -> 카피 데이터라 img1 변경에  영향 X
-
-    img1.setTo(Scalar(0, 255, 255));    //이미지1 yellow로 설정
-    img5 = ~img5;
-
-    imshow("img1", img1);
-    imshow("img2", img2);
-    imshow("img3", img3);
-    imshow("img4", img4);
-    imshow("img5", img5);
-
-    waitKey();
-    destroyAllWindows();
-    return 0;*/
-
-    /*
-    Mat img1 = imread("cat.bmp");
-
-
-
-    Mat img2 = img1(Rect(220, 120, 340, 240));
-    Mat img3 = img1(Rect(220, 120, 340, 240)).clone();
-
-    img2 = ~img2;
-
-    imshow("img1", img1);
-    imshow("img2", img2);
-    imshow("img3", img3);
-
-    waitKey();
-    destroyAllWindows();
-
-    */
-    /*
-    Mat mat1 = Mat::zeros(3, 4, CV_8UC1);
-    //cout << mat1 << endl;
-    
-    
-    
-    for (int j = 0; j < mat1.rows; j++)
-    {
-        uchar* p = mat1.ptr<uchar>(j);
-        for (int i = 0; i < mat1.cols; i++)
-        {
-            p[i]++;
-        }
-    }
-    cout << mat1 << endl;
-    
-    
-    for (int j = 0; j < 3; j++)
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            mat1.at<uchar>(j, i)++;
-        }
-    }
-    cout << mat1 << endl;
-    */
-    /*
-    Mat matA = Mat::zeros(3, 4, CV_8UC1);
-    for (int j = 0; j < matA.rows; j++)
-    {
-        for (int i = 0; i < matA.cols; i++)
-        {
-            matA.data[matA.cols * j + i];
-        }
-    }
-    
-    int j = 3;
-    Mat matB = Mat::zeros(3, 4, CV_8UC1);
-    uchar b = matB.data[matB.cols * j + 0];
-    cout << b << endl;
-    return 0;
-    */
-
-    Mat img1 = imread("lenna256.bmp", 0);
-
-
-    img1.data[img1.cols * (img1.rows / 2) + (img1.cols / 2)] = 0;
-    
-    imwrite("test.bmp", img1);
-
-    waitKey();
-    return 0;
-    
-
+	return 0;
 }
-
 
 /*
-Mat 클래스 : 행렬을 표현
-한 개 이상의 채널을 가질 수 있음
+	역행렬이 존재하는 일반적인 행렬이라면 가우스 소거법을 사용하는 DECOMP_LU를 사용할 수 있다. 이 값은 기본값으로 지정되어 있으므로 생략 가능
+	역행렬이 존재하지 않는 경우 DECOMP_SVD를 지정하면 singulat value decomposiotion 방법을 이용하여 역행렬을 구할 수 있음
 
-Class Mat
-{
-public:
-    Mat(int rows, int cols, int type);
-    Mat(Size size, int type);
-    Mat(int rows, int cols, const Scalar& s);
-    Mat(Size size, int type, const Scalar& s);
-    Mat(Size size, int type, Scalar& s);
-    Mat(const Mat& m);
-}
-U: 부호없는 정수형
-S: 부호있는 정수형 
-F: 부동 소수형  
-숫자는 비트  수
+	reshape 함수 : reshape(변경 할 채널 수, 변경 할 행의 수) 채널 수 가 0이면 채널 변경 없음
+	resize 함수 : resize(변경 할 행의 수, 빈공간을 채울 숫자)
 
- #define CV_8U  unsigned char   
- #define CV_8S  signed char     
- #define CV_16U unsigned short  
- #define CV_16S signed short    
- #define CV_32S int
- #define CV_32F float
- #define CV_64F double
- #define CV_16F float16_t
+	Vec 클래스 Matx클래스를 상속받아 만들어진 클래스, 열 개수가 1개로 특화된 벡터 표현 클래스
+	Vec<uchar, 3> p1, p2(0, 0, 255);
 
- BGR 순서 세 개의 색상 성분을 가지고 있는 컬러 영상은 
- unsigned char 자료형을 사용하고 세 개의 채널을 가지고 있기 때문에 CV_8UC3 타입
-
- 복소수처럼 두 개의 실수 값을 사용하는 행렬은 CV_32FC2 타입으로 만들 수 있음 
-
- Mat img(480, 640, CV_8UC3, Scalar(0, 0, 255)) => red
-
- Mat mat = Mat::eye(3, 3, CV_32FC1); => identity matrix
-
- float data[] = {1, 2, 3, 4, 5, 6};
- Mat mat(2, 3, CV_32FC1, data);
- => data의 값이 행부터 채움
-
-
-Mat::setTo() 함수: 두 개의 인자를 가지고 있지만 두 번째 인자 mask는 기본값을 가지고 있으므로 생략가능
-
-
-부분행렬 추출 주의사항 메모리를 공유한다
-img = ~img : 반전이미지
-
- Mat::at() 함수
- mat1.at<uchar>(j, i)++;
- mat1은 0으로 초기화 된 3x4 행렬
-
- Mat mat1 - Mat::zeros(3, 4, CV_8UC1);
-
- matA.data[matA.cols*j + i];
 
 
 */
-
-
